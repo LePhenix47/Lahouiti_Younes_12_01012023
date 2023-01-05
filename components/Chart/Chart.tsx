60f8101b09aeb85f37a407c35be1a77838f11790
Younes Lahouiti <loller763@gmail.com>
2023-01-05 11:42:30 +0100

    Débug de l'UI des SVG pour Chrome

--- components/Chart/Chart.tsx
+++ components/Chart/Chart.tsx
@@ -2,10 +2,15 @@
 import React from "react";
 
 export default function Chart({ chartType, data }: any) {
+  const chartIsBarChart = chartType.includes("bar");
+
   return (
     <section className={`chart chart__${chartType}`}>
       {chartType}
-      <svg></svg>
+      <svg
+        viewBox={`0 0 100 ${chartIsBarChart ? "35" : "100"}`}
+        xmlns="http://www.w3.org/2000/svg"
+      ></svg>
     </section>
   );
 }
