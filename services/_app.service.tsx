cc82b208405074e983a335156e040775f4e18d57
Younes Lahouiti <loller763@gmail.com>
2023-01-13 18:12:57 +0100

    Changements des url dans le service d'appels API pour Vercel

--- services/_app.service.tsx
+++ services/_app.service.tsx
@@ -12,30 +12,42 @@ export default class AppService {
   constructor() {
     //For the deployment on vercel, it checks if
     this.urlAPI = "http://localhost:4200/user";
-    // this.urlAPI = "https://p12-api.onrender.com/user";
   }
 
   //  Retrieves information from the user
   getUserInfo(id: number): dataType {
-    const data: dataType = useFetch(`${this.urlAPI}/${id}`);
+    // const data: dataType = useFetch(`${this.urlAPI}/${id}`);
+    const data: dataType = useFetch("http://localhost:3000/api/user/data");
+
     return data;
   }
 
   // Retrieves the user's activity day by day with kilograms and calories.
   getActivityInfo(id: number): dataType {
-    const data: dataType = useFetch(`${this.urlAPI}/${id}/activity`);
+    // const data: dataType = useFetch(`${this.urlAPI}/${id}/activity`);
+    const data: dataType = useFetch(
+      "http://localhost:3000/api/user/activity/data"
+    );
     return data;
   }
 
   // Retrieves the average sessions of the user per day. The week starts on Monday.
   getSessionsInfo(id: number): dataType {
-    const data: dataType = useFetch(`${this.urlAPI}/${id}/average-sessions`);
+    // const data: dataType = useFetch(`${this.urlAPI}/${id}/average-sessions`);
+    const data: dataType = useFetch(
+      "http://localhost:3000/api/user/average-sessions/data"
+    );
+
     return data;
   }
 
   // Retrieves the user's performance (energy, endurance, etc.).
   getPerformanceInfo(id: number): dataType {
-    const data: dataType = useFetch(`${this.urlAPI}/${id}/performance`);
+    // const data: dataType = useFetch(`${this.urlAPI}/${id}/performance`);
+    const data: dataType = useFetch(
+      "http://localhost:3000/api/user/performance/data"
+    );
+
     return data;
   }
 }
